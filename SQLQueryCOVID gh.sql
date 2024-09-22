1afe0b30a1f2bd185de5fb715fc508e8fe9425f5
Ezima Christiana <Christyezima@gmail.com>
2024-09-22 13:19:46 +0200

    Add files via upload

--- SQLQueryCOVID gh.sql
+++ SQLQueryCOVID gh.sql
@@ -12,94 +12,71 @@ SELECT location,date,total_cases,new_cases,total_deaths,population
 FROM PORTFOLIOPROJECTO1..Coviddeaths
 ORDER BY 1,2
 
---TOTALCASES VS TOTALDEATHS
+--TOTALCASES VS TOTALDEATHS GLOBALLY
 SELECT location,date,total_cases,total_deaths,(total_deaths/NULLIF(total_cases,0))*100 AS Deathpercentage
 FROM PORTFOLIOPROJECTO1..Coviddeaths
 ORDER BY 1,2
 
---TOTALCASES VS TOTALDEATHS IN AFRICA
-SELECT location,date,total_cases,total_deaths,(total_deaths/NULLIF(total_cases,0))*100 AS Deathpercentage
+--TOTALCASES VS TOTALDEATHS IN AFRICA 
+SELECT SUM(total_cases) AS Total_cases,
+SUM(total_deaths) AS Total_deaths,(SUM(total_deaths)/SUM(total_cases)) * 100 AS Deathpercentage
 FROM PORTFOLIOPROJECTO1..Coviddeaths
-WHERE location LIKE '%Africa%'
+WHERE location ='Africa'
 ORDER BY 1,2
 
---TOTALCASES VS POPULATION
-SELECT location,date,total_cases,population,(NULLIF(total_cases,0)/population)*100 AS Deathpercentage
+--TOTALCASES VS POPULATION IN AFRICA
+SELECT SUM(total_cases) AS Total_cases,
+SUM(population) AS Total_population,(SUM(population)/SUM(total_cases)) * 100 AS Casepercentage
 FROM PORTFOLIOPROJECTO1..Coviddeaths
-WHERE location LIKE '%Africa%'
-ORDER BY 1,2
+WHERE continent='Africa'
 
 --COUNTRIES WITH HIGHEST INFECTION COMPARED TO POPULATION
 SELECT location,population,MAX(total_cases) AS Highestinfectioncount, MAX((NULLIF(total_cases,0)/population))*100 AS
 Percentageofinfectedpopulation
 FROM PORTFOLIOPROJECTO1..Coviddeaths
-WHERE location LIKE '%Africa%'
+WHERE continent= 'Africa'
 GROUP BY location,population
 ORDER BY Percentageofinfectedpopulation DESC
 
---COUNTRIES WITH HIGHEST MORTALITY RATE PER POPULATION
-
+--1 MORTALITY RATE IN AFRICA
 SELECT location,MAX(total_deaths) AS Totaldeathcount
 FROM PORTFOLIOPROJECTO1..Coviddeaths
-WHERE location LIKE '%Africa%' 
-AND continent IS NOT NULL
+WHERE continent='Africa'
 GROUP BY location
 ORDER BY Totaldeathcount DESC
 
---BY CONTINENT
-
-SELECT location,MAX(total_deaths) AS Totaldeathcount
+--2 Global death count across africa daily
+SELECT location,date,SUM(New_cases) AS TotalNewCases,SUM(total_deaths) AS TotalDeaths,
+CASE WHEN SUM(total_cases)>0 THEN(SUM(total_deaths)/SUM(total_cases)) * 100 ELSE 0 END AS Deathpercentage
 FROM PORTFOLIOPROJECTO1..Coviddeaths
-WHERE location LIKE '%Africa%' 
-AND continent IS NULL
-GROUP BY location
-ORDER BY Totaldeathcount DESC
+WHERE continent='Africa' 
+GROUP BY location,date
+ORDER BY location,date;
 
---Global deathcount
-SELECT date,SUM(New_cases)--,total_deaths,(total_deaths/total_cases)*100 AS Deathpercentage
-FROM PORTFOLIOPROJECTO1..Coviddeaths
-WHERE location LIKE '%Africa%' 
-AND continent IS NOT NULL
-GROUP BY date
-ORDER BY 1,2
-
---DEATHCASES RECORDED DAILY ACROSS AFRICA
-SELECT date,SUM(new_cases) AS total_cases,
-SUM(CAST(new_deaths as int)) AS total_deaths,
-(SUM(CAST(new_deaths as int)) / NULLIF(SUM(new_cases) ,0))* 100 AS Deathpercentage
-FROM PORTFOLIOPROJECTO1..Coviddeaths
-WHERE location LIKE '%Africa%' 
-AND continent IS NOT NULL
-GROUP BY date
-ORDER BY 1,2
-
---TOTAL CASES RECORDED IN AFRICA
-SELECT SUM(new_cases) AS total_cases,
-SUM(CAST(new_deaths as int)) AS total_deaths,
-(SUM(CAST(new_deaths as int)) / NULLIF(SUM(new_cases) ,0))* 100 AS Deathpercentage
-FROM PORTFOLIOPROJECTO1..Coviddeaths
-WHERE location LIKE '%Africa%' 
-AND continent IS NOT NULL
-GROUP BY date
-ORDER BY 1,2
-
---VACCINATION PER TOTAL POPULATION
-
-SELECT Cd.continent,Cd.location,Cd.date,Cd.population,Cv.new_vaccinations
-FROM PORTFOLIOPROJECTO1..Coviddeaths Cd
-JOIN PORTFOLIOPROJECTO1..Covidvaccination Cv
-ON Cd.location=Cv.location
-AND Cd.date=Cd.date
-WHERE Cd.continent IS NOT NULL
+--3 TOTAL VACCINATION BY POPULATION
+SELECT cd.location,cd.date,SUM(cd.new_cases) AS Total_cases,SUM(cd.total_deaths) AS Total_deaths,
+CASE WHEN SUM(cd.total_cases) > 0
+THEN (SUM(cd.total_deaths) / SUM(cd.total_cases)) *100
+ELSE 0
+END AS DeathPercentsge,SUM(CAST(cv.total_vaccinations AS INT)) AS TotalVaccinations 
+FROM PORTFOLIOPROJECTO1..Coviddeaths cd
+JOIN
+PORTFOLIOPROJECTO1..Covidvaccination cv
+ON
+cd.location=cv.location AND cd.date=cv.date
+WHERE cd.continent='Africa'
+GROUP BY cd.location,cd.date
+ORDER BY  cd.location,cd.date;
 
 --NEW VACCINATION PER DAY
-SELECT Cd.continent,Cd.location,Cd.date,Cd.population,Cv.new_vaccinations,
-SUM(Cast(Cv.new_vaccinations as int)) OVER (Partition by Cd.location)
-FROM PORTFOLIOPROJECTO1..Coviddeaths Cd
-JOIN PORTFOLIOPROJECTO1..Covidvaccination Cv
-ON Cd.location=Cv.location
-AND Cd.date=Cd.date
-WHERE Cd.continent IS NOT NULL
+
+WITH DailyVaccinations AS (SELECT date,SUM(CAST(total_vaccinations AS INT)) AS TotalVaccinationsPerDay 
+FROM PORTFOLIOPROJECTO1..Covidvaccination
+GROUP BY date )
+SELECT date,TotalVaccinationsPerDay-COALESCE(LAG(TotalVaccinationsPerDay) 
+OVER(ORDER BY date),0) AS NewVaccinationPerDay
+FROM DailyVaccinations
+ORDER BY date;
 
 
 
